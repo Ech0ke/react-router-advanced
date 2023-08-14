b73de97f90ef0305ce3a44814562942e14513de1
Ech0ke <aidas.zygas@gmail.com>
2023-08-14 14:02:18 +0300

    Added helper function to post new post data to api

--- src/helpers/api/createPost.ts
+++ src/helpers/api/createPost.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { API_POSTS } from "../../../api/urls/apiUrls";
+import { NewPostType } from "../../types/newPostType";
 import { PostType } from "../../types/postType";
 
 export async function createPost(
-  formData: { [k: string]: FormDataEntryValue },
+  formData: NewPostType,
   options: AxiosRequestConfig
 ): Promise<PostType> {
   const res = await axios.post(API_POSTS, formData, options);
