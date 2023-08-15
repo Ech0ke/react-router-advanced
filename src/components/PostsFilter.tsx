09d9e1528e7e336e00076943136f69584189fed6
Ech0ke <aidas.zygas@gmail.com>
2023-08-15 13:40:18 +0300

    userRef -> userIdRef, userList is dynamically loaded from api

--- src/components/PostsFilter.tsx
+++ src/components/PostsFilter.tsx
@@ -15,14 +15,14 @@ function PostsFilter({
   const isLoading = state === "loading";
 
   const queryRef = useRef<HTMLInputElement>(null!);
-  const userRef = useRef<HTMLSelectElement>(null!);
+  const userIdRef = useRef<HTMLSelectElement>(null!);
 
   useEffect(() => {
     queryRef.current.value = query;
   }, [query]);
 
   useEffect(() => {
-    userRef.current.value = userId;
+    userIdRef.current.value = userId;
   }, [userId]);
 
   return (
@@ -34,19 +34,13 @@ function PostsFilter({
         </div>
         <div className="form-group">
           <label htmlFor="userId">Author</label>
-          {/* TODO: Users retreived from db */}
-          <select name="userId" id="userId" ref={userRef}>
+          <select name="userId" id="userId" ref={userIdRef}>
             <option value="">Any</option>
-            <option value="1">Leanne Graham</option>
-            <option value="2">Ervin Howell</option>
-            <option value="3">Clementine Bauch</option>
-            <option value="4">Patricia Lebsack</option>
-            <option value="5">Chelsey Dietrich</option>
-            <option value="6">Mrs. Dennis Schulist</option>
-            <option value="7">Kurtis Weissnat</option>
-            <option value="8">Nicholas Runolfsdottir V</option>
-            <option value="9">Glenna Reichert</option>
-            <option value="10">Clementina DuBuque</option>
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
         </div>
         <button disabled={isLoading} className="btn">
